d4ee3673a53ed94885e895dfda2206a5ccd58e60
lianxinbo <lianxinbo@hengli.com>
2022-03-03 14:19:34 +0800

    初始话

---
+++ Operation/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Operation
+//
+//  Created by lian on 2022/3/3.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
